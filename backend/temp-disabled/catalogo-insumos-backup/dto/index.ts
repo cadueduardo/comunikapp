a9dbebb00e89f2e976e28be01bf274f8ce0deb68
cadueduardo <cadu.eduardo@gmail.com>
2026-05-22 14:54:25 -0300

    Remove tracked backup artifacts

--- backend/temp-disabled/catalogo-insumos-backup/dto/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './create-catalogo-insumo.dto';
-export * from './update-catalogo-insumo.dto';
-export * from './buscar-insumos.dto';
--- backend/temp-disabled/catalogo-insumos-backup/dto/update-catalogo-insumo.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCatalogoInsumoDto } from './create-catalogo-insumo.dto';
-
-export class UpdateCatalogoInsumoDto extends PartialType(CreateCatalogoInsumoDto) {}
